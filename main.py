65d2df11d89329a65ce9724e91e136a844d4b9d6
ahihiyou20 <95456402+ahihiyou20@users.noreply.github.com>
2022-02-22 16:58:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import requests
+username = input("Enter Your Username: ")
+token = input("Enter Authorize Token: ")
+r = requests.get('https://testapi.io/api/ahihiyou20/authorize')
+try:
+ if token == r.json()[username]:
+  print("Successfuly Logged In !")
+ else:
+  print("Invalid Token")
+  exit()
+except KeyError:
+ print("Invalid Username")
+ exit()
 import time
 import os
 from sys import executable, argv
@@ -6,7 +19,6 @@ from multiprocessing import Process
 from multiprocessing import Pool
 import random
 import re
-import requests
 try:
  from inputimeout import inputimeout,TimeoutOccurred
 except:
